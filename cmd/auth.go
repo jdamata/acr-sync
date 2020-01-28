3928f85ba74b0eec5bb3c83b13600837d25265af
Joel Damata <joel.damata@pointclickcare.com>
2020-01-27 23:38:26 -0500

    wip

---
+++ cmd/auth.go
@@ -0,0 +1,92 @@
+package cmd
+
+import (
+	"encoding/base64"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"os/user"
+	"strings"
+
+	"github.com/docker/docker/api/types"
+	log "github.com/sirupsen/logrus"
+)
+
+// https://github.com/Azure/acr/blob/master/docs/AAD-OAuth.md#authenticating-docker-with-an-acr-refresh-token
+
+// AzureConfig struct used to unmarshal ~/.azure/accessTokens.json
+type AzureConfig []struct {
+	ExpiresIn        int    `json:"expiresIn"`
+	Authority        string `json:"_authority"`
+	RefreshToken     string `json:"refreshToken"`
+	TokenType        string `json:"tokenType"`
+	ExpiresOn        string `json:"expiresOn"`
+	UserID           string `json:"userId"`
+	IsMRRT           bool   `json:"isMRRT"`
+	ClientID         string `json:"_clientId"`
+	Resource         string `json:"resource"`
+	AccessToken      string `json:"accessToken"`
+	IdentityProvider string `json:"identityProvider,omitempty"`
+	Oid              string `json:"oid,omitempty"`
+}
+
+// ACRToken retrieved from
+type ACRToken struct {
+	Token string `json:"refresh_token"`
+}
+
+func parseAzureConfig() (string, string, string) {
+	var accessToken string
+	var refreshToken string
+	var tenantID string
+	var expiresOn string
+	var azureConfig AzureConfig
+	usr, _ := user.Current()
+	homedir := usr.HomeDir
+	authenticationFile, err := os.Open(homedir + "/.azure/accessTokens.json")
+	if err != nil {
+		log.Fatalf("Failed to open ~/.azure/accessTokens.json. Be sure to run az login first! %v\n", err)
+	}
+	byteValue, _ := ioutil.ReadAll(authenticationFile)
+	json.Unmarshal(byteValue, &azureConfig)
+	for i := 0; i < len(azureConfig); i++ {
+		if azureConfig[i].ExpiresOn > expiresOn {
+			expiresOn = azureConfig[i].ExpiresOn
+			accessToken = azureConfig[i].AccessToken
+			refreshToken = azureConfig[i].RefreshToken
+			tenantID = strings.TrimPrefix(azureConfig[i].Authority, "https://login.microsoftonline.com/")
+		}
+	}
+	return accessToken, refreshToken, tenantID
+}
+
+func genACRToken(accessToken string, refreshToken string, acrRepo string, tenantID string) ACRToken {
+	var acrToken ACRToken
+	data := url.Values{}
+	data.Set("grant_type", "access_token_refresh_token")
+	data.Set("access_token", accessToken)
+	data.Set("refresh_token", refreshToken)
+	data.Set("service", acrRepo)
+	data.Set("tenant", tenantID)
+	resp, err := http.PostForm("https://"+acrRepo+"/oauth2/exchange", data)
+	if err != nil {
+		log.Fatal(err, "Authenticate via the cli with az login first")
+	}
+	byteValue, _ := ioutil.ReadAll(resp.Body)
+	json.Unmarshal(byteValue, &acrToken)
+	return acrToken
+}
+
+func genDockerAuth(acrToken string) string {
+	authConfig := types.AuthConfig{
+		Username: "00000000-0000-0000-0000-000000000000",
+		Password: acrToken,
+	}
+	encodedJSON, err := json.Marshal(authConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return base64.URLEncoding.EncodeToString(encodedJSON)
+}
--- cmd/root.go
+++ cmd/root.go
@@ -1,35 +1,15 @@
 package cmd
 
 import (
-	"os"
-	"os/user"
-	"io/ioutil"
-	"encoding/base64"
-	"encoding/json"
+	// "fmt"
 
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
+	"github.com/docker/docker/api/types"
 	"golang.org/x/net/context"
 )
-
-// AccessToken struct used to unmarshal ~/.azure/accessTokens.json
-type AccessToken []struct {
-	ExpiresIn        int    `json:"expiresIn"`
-	Authority        string `json:"_authority"`
-	RefreshToken     string `json:"refreshToken"`
-	TokenType        string `json:"tokenType"`
-	ExpiresOn        string `json:"expiresOn"`
-	UserID           string `json:"userId"`
-	IsMRRT           bool   `json:"isMRRT"`
-	ClientID         string `json:"_clientId"`
-	Resource         string `json:"resource"`
-	AccessToken      string `json:"accessToken"`
-	IdentityProvider string `json:"identityProvider,omitempty"`
-	Oid              string `json:"oid,omitempty"`
-}
 
 var (
 	rootCmd  = &cobra.Command{
@@ -55,43 +35,8 @@ func Execute(version string) error {
 	return rootCmd.Execute()
 }
 
-func grabToken() string {
-	var token string
-	var expireson string
-	usr, _ := user.Current()
-	homedir := usr.HomeDir
-	accessTokensFile, err := os.Open(homedir + "/.azure/accessTokens.json")
-	if err != nil {
-		log.Fatalf("Failed to open ~/.azure/accessTokens.json. Be sure to run az login first! %v\n", err)
-	}
-	byteValue, _ := ioutil.ReadAll(accessTokensFile)
-	var accesstoken AccessToken
-	json.Unmarshal(byteValue, &accesstoken)
-	for i := 0; i < len(accesstoken); i++ {
-		if accesstoken[i].ExpiresOn > expireson {
-			expireson = accesstoken[i].ExpiresOn
-			token = accesstoken[i].AccessToken
-		}
-	}
-	return token
-}
-
-func dockerCredential() string {
-	token := grabToken()
-	authConfig := types.AuthConfig{
-		Username: "<token>",
-		Password: token,
-	}
-	encodedJSON, err := json.Marshal(authConfig)
-	if err != nil {
-		log.Fatal(err)
-	}
-	authStr := base64.URLEncoding.EncodeToString(encodedJSON)
-	return authStr
-}
-
 func dockerClient(acrrepo string) *client.Client {
-	cli, err := client.NewClientWithOpts(client.WithHost(acrrepo), client.FromEnv, client.WithAPIVersionNegotiation())
+	cli, err := client.NewClientWithOpts(client.WithHost(acrrepo), client.WithAPIVersionNegotiation(), client.WithVersion("1.39"))
 	if err != nil {
 		log.Fatalf("Failed to create docker client. %v\n", err)
 	}
@@ -99,17 +44,13 @@ func dockerClient(acrrepo string) *client.Client {
 }
 
 func main(cmd *cobra.Command, args []string) {
+	accessToken, refreshToken, tenantID := parseAzureConfig()
+	acrToken := genACRToken(accessToken, refreshToken, args[0], tenantID)
+	docker := dockerClient("https://" + args[0])
 
-	// Authenticate to both docker registries
-	acrSrcURL := "https://" + args[0] // + "/v2/"
-	// acrDestURL := "https://" + args[1]
-	srcClient := dockerClient(acrSrcURL)
-	// destClient := dockerClient(acrDestURL)
-
-	// Grab docker credential
-	// authStr := dockerCredential()
 	ctx := context.Background()
-	_, err := srcClient.ImageList(ctx, types.ImageListOptions{All: true})
+	//_, err := docker.ImageList(ctx, types.ImageListOptions{All: true})
+	_, err := docker.ImagePull(ctx, "azeng.azurecr.io/azuretls_test:latest", types.ImagePullOptions{RegistryAuth: acrToken.Token})	
 	if err != nil {
 		log.Fatal(err)
 	}
